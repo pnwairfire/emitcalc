ec2e5f08a7d4d01eba8b1b338d7ad1ed36634753
Joel Dubowy <jdubowy@gmail.com>
2015-02-20 15:14:50 -0800

    Initial commit - skeleton package

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from pip.req import parse_requirements
+
+# Note: using pip.req.parse_requirements like so:
+#  > REQUIREMENTS = [str(ir.req) for ir in parse_requirements('requirements.txt')]
+# results in the folloing error on Heroku:
+#    TypeError: parse_requirements() missing 1 required keyword argument: 'session'
+with open('requirements.txt') as f:
+    REQUIREMENTS = f.read().splitlines()
+
+setup(
+    name='emitcalc',
+    version='0.1.0',
+    author='Joel Dubowy',
+    author_email='jdubowy@gmail.com',
+    packages=[
+        'emitcalc'
+    ],
+    scripts=[
+        'bin/emitcalc'
+    ],
+    package_data={
+    },
+    url='git@bitbucket.org:fera/airfire-emissions-calculator.git',
+    description='Package providing a calculator for computing emissions from consume output.',
+    install_requires=REQUIREMENTS,
+)
