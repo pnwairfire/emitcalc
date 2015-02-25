9f1fcae9522cd2e70e9a428c98b6fcaa26745b70
Joel Dubowy <jdubowy@gmail.com>
2015-02-24 17:43:17 -0800

    add test stubs

--- test/emitcalc/test_calculator.py
+++ test/emitcalc/test_calculator.py
@@ -3,5 +3,12 @@ __copyright__   = "Copyright 2014, AirFire, PNW, USFS"
 
 #from py.test import raises
 
+from emitcalc.calculator import EmissionsCalculator
+
 class EmissionsCalculatorTest:
-    pass
+
+    def test_bad_calls(self):
+        pass
+
+    def test_varying_species(self):
+        pass
