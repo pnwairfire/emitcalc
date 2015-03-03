9fdd58e128bd674d83043a1c61fd220ba4872d8a
Joel Dubowy <jdubowy@gmail.com>
2015-03-02 16:42:50 -0800

    Switch from distutils to setuputils and fix parsing of arguments in setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,50 @@
-from distutils.core import setup
-from pip.req import parse_requirements
+import re
+from setuptools import setup, find_packages
 
 # Note: using pip.req.parse_requirements like so:
 #  > REQUIREMENTS = [str(ir.req) for ir in parse_requirements('requirements.txt')]
 # results in the folloing error on Heroku:
 #    TypeError: parse_requirements() missing 1 required keyword argument: 'session'
-with open('requirements.txt') as f:
-    REQUIREMENTS = f.read().splitlines()
+git_url_matcher = re.compile('^git\+.+/([^/.]+)(\.git)?@v?([0-9.]+)$')
+def parse_requirements(dep_links, req_file_name):
+    with open(req_file_name) as f:
+        reqs = []
+        for r in f.read().splitlines():
+            m = git_url_matcher.match(r)
+            if m:
+                dep_links.append(r)
+                reqs.append("%s==%s" % (m.group(1), m.group(3)))
+            else:
+                reqs.append(r)
+        return reqs
+
+dependency_links = []
+requirements = parse_requirements(dependency_links, 'requirements.txt')
+test_requirements = parse_requirements(dependency_links, 'requirements-test.txt')
 
 setup(
     name='emitcalc',
     version='0.1.2',
+    license='MIT',
     author='Joel Dubowy',
     author_email='jdubowy@gmail.com',
-    packages=[
-        'emitcalc'
-    ],
+    packages=find_packages(),
     scripts=[
         'bin/emitcalc'
     ],
     package_data={
     },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS"
+    ],
     url='https://github.com/pnwairfire/emitcalc',
     description='Package providing a calculator for computing emissions from consume output.',
-    install_requires=REQUIREMENTS,
+    install_requires=requirements,
+    dependency_links=dependency_links,
+    tests_require=test_requirements
 )
